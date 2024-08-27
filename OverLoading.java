b28e46e043422ee59c380c087a3949a5a84ba685
Vijay123432 <vijaypenugonda03@gmail.com>
2024-08-27 20:37:13 +0530

    Update OverLoading.java

--- OverLoading.java
+++ OverLoading.java
@@ -1,3 +1,18 @@
 public class OverLoading{
 // Prepare an Example to show OverLoading Concept
+
+public void kl(String name){
+    System.out.println(name);
 }
+public void kl(int age){
+    System.out.println(age);
+}
+public static void main(String[] args) {
+    OverLoading vr=new OverLoading();
+    
+    vr.kl(21);
+    vr.kl("vijay");
+
+}
+}
+
