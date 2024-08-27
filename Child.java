c7ea009c4c0cda1881bd81b5e1c2d482f51e0829
Vijay123432 <vijaypenugoneda03@gmail.com>
2024-08-27 19:56:28 +0530

    child

--- Child.java
+++ Child.java
@@ -5,7 +5,7 @@ public class Child extends Parent{
  }
  public static void main(String[] args) {
    Child c=new Child();
-    System.out.println(c.sname("vijay"));
-    
+    System.out.println(c.sname("marsh"));
+      System.out.println(c.cname("mello"));
  }
 }
--- Child1.java
+++ Child1.java
@@ -1,3 +1,12 @@
-public class Child1{
+public class Child1 extends Parent{
 // Prepare Some Function and extend to Parent Class
+public String d1name(String name2){
+    return name2;
+ }
+ public static void main(String[] args) {
+   Child d=new Child();
+    System.out.println(d.sname("vijay"));
+    
+    
+ }
 }
