83a2c791c55404389a9e617c48488146415b613d
Vijay123432 <vijaypenugonda03@gmail.com>
2024-08-27 20:36:05 +0530

    Update Child1.java

--- Child1.java
+++ Child1.java
@@ -1,3 +1,13 @@
-public class Child1{
+public class Child1 extends Parent{
 // Prepare Some Function and extend to Parent Class
+public String dname(String name2){
+    return name2;
+ }
+ public static void main(String[] args) {
+   Child1 d=new Child1();
+    System.out.println(d.sname("marsh"));
+        System.out.println(d.dname("vijay"));
+
+    
+ }
 }
